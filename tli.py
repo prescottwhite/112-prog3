f3d98323bb4a2ca487ab0546ae0d1eef28a20258
Prescott White <prgwhite@ucsc.edu>
2019-11-14 20:55:20 -0800

    Build basic symTable and stmtList from parsed statements

--- tli.py
+++ tli.py
@@ -2,17 +2,9 @@
 import fileinput
 import sys
 
-# read 1st argument when calling script
-fileName = sys.argv[1]
-
-# open file with given filename
-file = open(fileName, "r")
-for x in file:
-	print(x)
-
 # used to store a parsed TL expressions which are
 # constant numbers, constant strings, variable names, and binary expressions
-class Expr :
+class Expr:
     def __init__(self,op1,operator,op2=None):
         self.op1 = op1
         self.operator = operator
@@ -32,8 +24,9 @@ class Expr :
             return 0
 
 # used to store a parsed TL statement
-class Stmt :
-    def __init__(self,keyword,exprs):
+class Stmt:
+    def __init__(self,lineNum,keyword,exprs):
+        self.lineNum = lineNum
         self.keyword = keyword
         self.exprs = exprs
 
@@ -46,3 +39,47 @@ class Stmt :
     # perform/execute this statement given the environment of the symTable
     def perform(self, symTable):
         print ("Doing: " + str(self))
+
+# read 1st argument when calling script
+fileName = sys.argv[1]
+
+# open file with given filename
+file = open(fileName, "r")
+
+lineNum = 0
+symTable = {}
+stmtList = []
+def addStmt(stmtList, lineNum, keyword, lineParsed, stInd, endInd):
+    stmtList.append(Stmt(lineNum, keyword, lineParsed[stInd:endInd]))
+
+for x in file:
+    labelIndex = 0
+    lineNum += 1
+    lineParsed = x.split()
+    numTokens = len(lineParsed)
+	
+    # check if there is a label
+    if lineParsed[0].endswith(':'):
+        # store label without colon
+        symTable[lineParsed[0][:-1]] = lineNum
+        # rest of statement will start one index over
+        labelIndex = 1
+		
+    keyword = lineParsed[0 + labelIndex]
+    if keyword == "let":
+        addStmt(stmtList, lineNum, keyword, lineParsed, (1 + labelIndex), numTokens)
+        # stmtList.append(Stmt(lineNum, keyword, lineParsed[(1 + labelIndex):(numTokens)]))
+    elif keyword == "if":
+        addStmt(stmtList, lineNum, keyword, lineParsed, (1 + labelIndex), numTokens)
+        # stmtList.append(Stmt(lineNum, keyword, lineParsed[(1 + labelIndex):(numTokens)]))
+    elif keyword == "print":
+        addStmt(stmtList, lineNum, keyword, lineParsed, (1 + labelIndex), numTokens)
+        # stmtList.append(Stmt(lineNum, keyword, lineParsed[(1 + labelIndex):(numTokens)]))
+    elif keyword == "input":
+        addStmt(stmtList, lineNum, keyword, lineParsed, (1 + labelIndex), numTokens)
+        # stmtList.append(Stmt(lineNum, keyword, lineParsed[(1 + labelIndex):(numTokens)]))
+	
+for x in symTable:
+    print(str(symTable[x]))
+for x in stmtList:
+    print(str(x))
